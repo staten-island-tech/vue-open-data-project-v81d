4b2d5625152a3a4def1a97271761b86c77e7c502
v81d <v81d@proton.me>
2026-03-17 13:30:28 -0400

    feat: add styles

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+  ],
 });
 
 export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full h-full m-0 p-10 flex flex-col gap-10">
+    <h1 class="text-4xl font-display font-bold">Inspect the School</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
